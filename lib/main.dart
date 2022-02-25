d4b1833bcd932e140afd2e9254f14232e8db21f0
Johan Park <parkjohan@gmail.com>
2022-02-24 22:31:36 -1000

    installed firebase, and initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -24,8 +33,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Center(child: Text('Welcome'),),
+      home: Center(
+        child: Text('Welcome'),
+      ),
     );
   }
 }
-
