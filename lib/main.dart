d29ded31116af9762cf79d1c4bbcf4b7c8391869
Johan Park <parkjohan@gmail.com>
2022-02-27 23:51:25 -1000

    added simple fab component and entry list view screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'pages/entry_list_view.dart';
+
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
@@ -21,21 +22,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Center(
-        child: Text('Welcome'),
-      ),
+      theme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const EntryListView(),
+      },
     );
   }
 }
